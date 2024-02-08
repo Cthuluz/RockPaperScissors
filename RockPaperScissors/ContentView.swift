915ec0d5e505f206076fb3601aea2cf3a7649e2d
Isabel Nelson <inels19@wgu.edu>
2024-02-08 08:28:26 -0700

    Change buttons to imojis and delete title variable and title selection variable in place of ternary operator

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -13,20 +13,24 @@ struct ContentView: View {
             colors: [Color.indigo, Color.white],
             startPoint: .top, endPoint: .bottom)
         
-        var moves = ["rock", "paper", "scissors"]
-        var winOrLose = "Play to Win!"
+        let moves = ["rock", "paper", "scissors"]
+        var win = true
+        @State var selection = Int.random(in: 0...2)
+        @State var score = 0
         
         VStack {
             VStack (spacing: 30) {
-                Text(winOrLose)
-                Button("Rock") {}
-                Button("Paper") {}
-                Button("Scissors") {}
+                Text(win == true ? "Play to Win!" : "Play to Lose!")
+                Button("🪨") {}
+                Button("📃") {}
+                Button("✂️") {}
+                Text("Score: \(score)")
             }
             .frame(maxWidth: 300, maxHeight: 500)
             .background(.ultraThinMaterial)
             .cornerRadius(20)
             .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .font(.largeTitle)
         }
         .buttonStyle(.bordered)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
